b1dcf170ee0581d916eb98550c6b4140dae9eacb
Ic <iacopo.cutino@gmail.com>
2023-04-23 21:42:26 +0200

    aggiunto editor in edit, modifiche stile

--- resources/views/messages/edit.blade.php
+++ resources/views/messages/edit.blade.php
@@ -8,16 +8,13 @@
     <div class="container">
         <div class="mt-4">
             <div>
-                <form method="POST" action="{{ route('messages.update', $message) }}">
-                    <label for="textareaMessage" class="form-label">Message</label>
+                <form method="post" action="{{ route('messages.update', $message) }}">
                     @csrf
                     @method('patch')
-                    <textarea name="message" class="form-control" id="textareaMessage" rows="3" maxlength="255">{{ old('message', $message->message) }}</textarea>
+                    <textarea name="message" id="myeditorinstance" maxlength="255">{{ old('message', $message->message) }}</textarea>
+                    <button type="submit" class="btn btn-light mt-2">{{ __('Edit') }}</button>
                     <div class="d-inline">
-                    <button type="submit" class="btn btn-light mt-2">Edit</button>
-                    </div>
-                    <div class="d-inline">
-                    <a href="{{ route('messages.index') }}"><button type="button" class="btn btn-light mt-2 ms-3">Cancel</button></a>
+                        <a href="{{ route('messages.index') }}"><button type="button" class="btn btn-light mt-2 ms-3">Cancel</button></a>
                     </div>
                 </form>
             </div>
