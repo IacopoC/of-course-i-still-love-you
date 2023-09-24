7c1c00dca80a053c782dbcd91931a7d8a5c6a63b
Ic <iacopo.cutino@gmail.com>
2023-09-24 21:47:04 +0200

    aggiunta immagine nella dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,13 +7,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <h3 class="text-white">Welcome <strong>{{ Auth::user()->name }}</strong></h3>
-            <p class="text-white">Counter messages: <strong>{{ $count_messages }}</strong></p>
+        <div class="col-md-6">
+            <div class="pt-4">
+                <h3 class="text-white">Welcome <strong>{{ Auth::user()->name }}</strong></h3>
+            <p class="text-white pt-4">Counter messages: <strong>{{ $count_messages }}</strong></p>
             <p class="text-white">Email: {{ Auth::user()->email }}</p>
             <p class="text-white">Profile created at: {{ Auth::user()->created_at }}</p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="image">
+                <img src="{{ asset("img/achab.png") }}" class="img-fluid">
+            </div>
         </div>
     </div>
-    <div class="height-380"></div>
 </div>
 @endsection
