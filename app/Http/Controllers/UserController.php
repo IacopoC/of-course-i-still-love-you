a88597649ea4ab81960d9a5bf602af69aa240e70
Ic <iacopo.cutino@gmail.com>
2023-10-05 19:04:20 +0200

    modifiche checkbox 8.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,17 +9,15 @@ use App\Models\User;
 class UserController extends Controller
 {
 
-    public function store(Request $request, User $user): RedirectResponse
+    public function store(Request $request): RedirectResponse
     {
-
-        $this->authorize('update', $user);
         $validated = $request->validate([
             'trap' => 'boolean',
         ]);
 
         if($request->has('trap')) {
             $trapValue = $request->input('trap');
-            $trapValue->update();
+            $trapValue->update($validated);
         }
 
         return redirect(route('dashboard'));
