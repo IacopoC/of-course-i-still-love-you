79ac2f85ce345710a9dbf1a90f7a19ec1194407f
Ic <iacopo.cutino@gmail.com>
2023-10-05 19:02:08 +0200

    modifiche checkbox 7.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,22 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    public function store(Request $request): RedirectResponse
+
+    public function store(Request $request, User $user): RedirectResponse
     {
+
+        $this->authorize('update', $user);
         $validated = $request->validate([
             'trap' => 'boolean',
         ]);
 
-        $request->user()->update($validated);
-
+        if($request->has('trap')) {
             $trapValue = $request->input('trap');
-            $trapValue->save();
-            var_dump($trapValue);
-
+            $trapValue->update();
+        }
 
         return redirect(route('dashboard'));
     }
+
 }
