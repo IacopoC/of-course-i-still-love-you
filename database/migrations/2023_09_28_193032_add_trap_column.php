58e129578d7657502cfd3077db4cb71fb1f67092
Ic <iacopo.cutino@gmail.com>
2023-11-20 16:50:59 +0100

    bug registrazione utente.

--- database/migrations/2023_09_28_193032_add_trap_column.php
+++ database/migrations/2023_09_28_193032_add_trap_column.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table)
         {
-            $table->boolean('trap');
+            $table->boolean('trap')->default('0');
         });
     }
 
