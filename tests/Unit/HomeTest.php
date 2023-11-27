ff1e9ac33e69fc56e7468b0cc2be17d1c0548d09
Ic <iacopo.cutino@gmail.com>
2023-11-27 16:57:32 +0100

    rinomina file.

--- tests/Unit/HomeTest.php
+++ tests/Unit/HomeTest.php
@@ -4,14 +4,15 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+class HomeTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_that_true_is_true()
+
+    public function testFailure(): void
     {
         $this->assertTrue(true);
     }
