da82a67c5723d464bec8ee24621eccc701591467
Ic <iacopo.cutino@gmail.com>
2024-02-18 21:33:19 +0100

    modifiche controller.

--- app/Http/Controllers/UpdownController.php
+++ app/Http/Controllers/UpdownController.php
@@ -2,20 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Updown;
 
 class UpdownController extends Controller
 {
-    public function up($messageId): \Illuminate\Http\RedirectResponse
+    public function up($messageId): RedirectResponse
     {
         $upvote = new Updown();
         $upvote->up($messageId, Auth::id());
         return redirect()->back();
     }
 
-    public function down($messageId): \Illuminate\Http\RedirectResponse
+    public function down($messageId): RedirectResponse
     {
         $downvote = new Updown();
         $downvote->down($messageId, Auth::id());
