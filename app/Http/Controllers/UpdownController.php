f3449fbbe75514fbcb4d05ebd81aeeceefc4b2ab
Ic <iacopo.cutino@gmail.com>
2024-02-28 19:14:05 +0100

    correzione spaziature.

--- app/Http/Controllers/UpdownController.php
+++ app/Http/Controllers/UpdownController.php
@@ -13,13 +13,20 @@ class UpdownController extends Controller
         $upvote = new Updown();
         $upvote->up($messageId, Auth::id());
 
-        return redirect(route('messages.list'))->with(['upvote' => 'Up!', 'messageid' => $messageId]);
+        return redirect(route('messages.list'))->with([
+            'upvote' => 'Up!',
+            'messageid' => $messageId
+        ]);
     }
 
     public function down($messageId): RedirectResponse
     {
         $downvote = new Updown();
         $downvote->down($messageId, Auth::id());
-        return redirect(route('messages.list'))->with(['downvote' => 'Down!', 'messageid' => $messageId]);
+
+        return redirect(route('messages.list'))->with([
+            'downvote' => 'Down!',
+            'messageid' => $messageId
+        ]);
     }
 }
