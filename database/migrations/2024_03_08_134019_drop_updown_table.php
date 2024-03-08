185acdd545943441ea888b5d4c53889526e5f45c
Ic <iacopo.cutino@gmail.com>
2024-03-08 14:51:06 +0100

    tabella updown cancellata.

---
+++ database/migrations/2024_03_08_134019_drop_updown_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('updowns');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::create('updowns', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('message_id')->constrained()->cascadeOnDelete();
+            $table->string('updown');
+            $table->timestamps();
+        });
+    }
+};
