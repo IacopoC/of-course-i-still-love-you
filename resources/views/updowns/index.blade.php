8c627f84af03de117833b5b7484e813e657a674a
Ic <iacopo.cutino@gmail.com>
2024-03-09 22:34:05 +0100

    aggiunta nuova tabella updowns.

---
+++ resources/views/updowns/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title')
+    Updown - Of Course I still love you
+@endsection
+
+@section('description')
+    Write your short updown messages, read all your messages in the list.
+@endsection
+
+@section('content')
+@endsection
