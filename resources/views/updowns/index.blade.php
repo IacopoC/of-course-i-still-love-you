586d1bfc584839f7e0d3dbf977015d3be9ca0e13
Ic <iacopo.cutino@gmail.com>
2024-03-09 22:48:01 +0100

    aggiunta descrizione updown pagina.

--- resources/views/updowns/index.blade.php
+++ resources/views/updowns/index.blade.php
@@ -9,4 +9,11 @@
 @endsection
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Write short updown messages</h3>
+            </div>
+        </div>
+    </div>
 @endsection
