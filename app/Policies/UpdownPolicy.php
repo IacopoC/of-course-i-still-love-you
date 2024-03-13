5905ec3d240cb298844798b0eb9bbc1b63f30c15
Ic <iacopo.cutino@gmail.com>
2024-03-13 18:20:10 +0100

    aggiunti commenti.

--- app/Policies/UpdownPolicy.php
+++ app/Policies/UpdownPolicy.php
@@ -10,10 +10,26 @@ class UpdownPolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Updown  $updown
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+
     public function update(User $user, Updown $updown)
     {
         return $updown->user()->is($user);
     }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Updown  $updown
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
 
     public function delete(User $user, Updown $updown)
     {
