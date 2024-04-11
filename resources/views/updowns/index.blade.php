f58d3c4b1cc8791a25ca821b9f7f659a4c3582c9
Ic <iacopo.cutino@gmail.com>
2024-04-11 21:29:52 +0200

    correzione testo.

--- resources/views/updowns/index.blade.php
+++ resources/views/updowns/index.blade.php
@@ -16,7 +16,7 @@
                 <form method="post" action="{{ route('updowns.store') }}" class="mb-5">
                     @csrf
                 <div class="input-group mb-4 mt-4">
-                    <input type="text" class="form-control w-75" name="updown_message" aria-label="Text input with dropdown button" placeholder="Write here your short texts..." maxlength="155" required>
+                    <input type="text" class="form-control w-75" name="updown_message" aria-label="Text input with dropdown button" placeholder="Write here your short text..." maxlength="155" required>
                     <select class="form-select" aria-label="Default select example" name="updown" required>
                         <option value="Up">Up</option>
                         <option value="Down">Down</option>
