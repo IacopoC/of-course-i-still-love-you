61ffee7e3af6a3650e5c808257ac037e263c92f7
Ic <iacopo.cutino@gmail.com>
2025-01-08 11:25:57 +0100

    aggiunto factory updown.

---
+++ database/factories/UpdownFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Updown;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Updown>
+ */
+class UpdownFactory extends Factory
+{
+    protected $model = Updown::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'updown_message' => $this->faker->sentence,
+            'updown' => $this->faker->sentence,
+        ];
+    }
+}
