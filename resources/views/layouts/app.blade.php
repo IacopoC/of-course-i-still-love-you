9b7bba11490ab361164249005e0f1675b08a82e2
Ic <iacopo.cutino@gmail.com>
2025-01-19 11:55:07 +0100

    aggiunta allerta logout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,11 +64,28 @@
                         </p>
                     </li>
                     <li class="nav-item">
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                            @csrf
-                            <button class="nav-link" type="submit">Logout</button>
-                        </form>
+                        <a class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal">Logout</a>
                     </li>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Are you sure?</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Do you want to logout?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                        @csrf
+                                        <button class="btn btn-primary" type="submit">Logout</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 @endguest
             </ul>
         </header>
