17f26b48bf2502551efa1218f53ce3b4738f8288
Ic <iacopo.cutino@gmail.com>
2025-02-14 21:03:33 +0100

    modifica nome modale logout.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,13 +27,13 @@
             </p>
         </li>
         <li class="nav-item">
-            <button type="button" class="btn btn-secondary pb-2" data-bs-toggle="modal" data-bs-target="#exampleModal">Logout</button>
+            <button type="button" class="btn btn-secondary pb-2" data-bs-toggle="modal" data-bs-target="#logoutModal">Logout</button>
         </li>
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Are you sure?</h1>
+                        <h1 class="modal-title fs-5" id="logoutModalLabel">Are you sure?</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
