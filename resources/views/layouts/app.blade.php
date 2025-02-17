a9811a55668fb858295cd9221118cccd14bc99eb
Ic <iacopo.cutino@gmail.com>
2025-02-17 17:31:28 +0100

    modifica menu.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,14 +29,7 @@
 </head>
 <body>
     <div id="app">
-        <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
-            <a href="{{ url('/') }}" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                <span class="fs-4">Of Course I Still Love You</span>
-            </a>
-        @include('layouts.navigation')
-        </header>
-        </div>
+            @include('layouts.navigation')
         <main class="py-4">
             @yield('content')
         </main>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,53 +1,63 @@
-<ul class="nav nav-underline">
-    @guest
-        @if (Route::has('login'))
-            <li class="nav-item">
-                <a class="nav-link text-body-secondary {{ (request()->is('login')) ? 'active' : '' }}" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li>
-        @endif
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">Of Course I Still Love You</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link {{ (request()->is('login')) ? 'active' : '' }}" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
 
-        @if (Route::has('register'))
-            <li class="nav-item">
-                <a class="nav-link text-body-secondary {{ (request()->is('register')) ? 'active' : '' }}" href="{{ route('register') }}">{{ __('Register') }}</a>
-            </li>
-        @endif
-    @else
-        <li class="nav-item">
-            <a class="nav-link text-body-secondary {{ (request()->is('messages')) ? 'active' : '' }}" href="{{ route('messages.index') }}">{{ __('Create Messages') }}</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-body-secondary {{ (request()->is('updowns')) ? 'active' : '' }}" href="{{ route('updowns.index') }}">{{ __('Create Updowns') }}</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-body-secondary {{ (request()->is('dashboard')) ? 'active' : '' }}" href="{{ route('dashboard') }}">{{ __('Dashboard') }}</a>
-        </li>
-        <li class="nav-item">
-            <p class="nav-link text-body-secondary mb-0">
-                {{ Auth::user()->name }}
-            </p>
-        </li>
-        <li class="nav-item">
-            <button type="button" class="btn btn-secondary pb-2" data-bs-toggle="modal" data-bs-target="#logoutModal">Logout</button>
-        </li>
-        <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="logoutModalLabel">Are you sure?</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link {{ (request()->is('register')) ? 'active' : '' }}" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link {{ (request()->is('messages')) ? 'active' : '' }}" href="{{ route('messages.index') }}">{{ __('Create Messages') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ (request()->is('updowns')) ? 'active' : '' }}" href="{{ route('updowns.index') }}">{{ __('Create Updowns') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ (request()->is('dashboard')) ? 'active' : '' }}" href="{{ route('dashboard') }}">{{ __('Dashboard') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <p class="nav-link mb-0">
+                            {{ Auth::user()->name }}
+                        </p>
+                    </li>
+                    <li class="nav-item">
+                        <button type="button" class="btn btn-link pb-2" data-bs-toggle="modal" data-bs-target="#logoutModal">Logout</button>
+                    </li>
+                    <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="logoutModalLabel">Are you sure?</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Do you want to logout?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                        @csrf
+                                        <button class="btn btn-primary" type="submit">Logout</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="modal-body">
-                        Do you want to logout?
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                            @csrf
-                            <button class="btn btn-primary" type="submit">Logout</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                @endguest
+            </ul>
         </div>
-    @endguest
-</ul>
+    </div>
+</nav>
