be6590009bca06d0a4f0782e547042812e0db9cc
Ic <iacopo.cutino@gmail.com>
2025-03-21 18:02:56 +0100

    modifica layout login form.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,40 +5,39 @@
 @endsection
 
 @section('description')
-   Login to write and read all messages.
+   Login to see your dashboard and write and read all messages and updowns.
 @endsection
 
 @section('content')
-    <main class="form-signin w-100 m-auto">
+    <div class="form-signin w-100 m-auto">
         <form method="POST" action="{{ route('login') }}">
             @csrf
-            <h1 class="h3 my-3 fw-normal">Please sign in</h1>
-
+            <h1 class="h3 my-3 fw-normal">Please login</h1>
             <div class="form-floating">
-                <input type="email" class="form-control @error('email') is-invalid @enderror" id="floatingInput email" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="name@example.com">
+                <input type="email" class="form-control @error('email') is-invalid @enderror" id="floatingInput" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="name@example.com">
                 <label for="floatingInput">Email address</label>
+                @error('email')
+                <div class="py-2 invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
-            @error('email')
-            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-            @enderror
             <div class="form-floating">
-                <input type="password" class="form-control @error('password') is-invalid @enderror" id="floatingPassword password" placeholder="Password" name="password" required autocomplete="current-password">
+                <input type="password" class="form-control @error('password') is-invalid @enderror" id="floatingPassword" placeholder="Password" name="password" required autocomplete="current-password">
                 <label for="floatingPassword">Password</label>
+                @error('password')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
-            @error('password')
-            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-            @enderror
             <div class="form-check text-start my-3">
-                <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault remember" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" name="remember" {{ old('remember') ? 'checked' : '' }}>
                 <label class="form-check-label" for="flexCheckDefault">
                     Remember me
                 </label>
             </div>
-            <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+            <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
             @if (Route::has('password.request'))
                 <div class="py-4">
                 <a class="btn btn-link" href="{{ route('password.request') }}">
@@ -47,5 +46,5 @@
                 </div>
             @endif
         </form>
-    </main>
+    </div>
 @endsection
